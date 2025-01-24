38df52653bc9ca1a3968be12b9891cd47e9593b7
zinmin@hotmail.com <zinmin@hotmail.com>
2025-01-24 16:46:40 -0600

    Add project files.

---
+++ MB.BusinessLayer/Managers/ManarboardManager.cs
@@ -0,0 +1,31 @@
+﻿using MB.BusinessLayer.Managers.Interface;
+using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MB.BusinessLayer.Managers
+{
+    public class MaharboardManager : IManarboardManager
+    {
+        public async Task<string> Calculate(DateOnly date)
+        {
+            // Calculate the day of the week
+            // Default DayOfWeek value is 0 for Sunday, 1 for Monday.
+            // But Maharboard starts from 1 for Sunday, 2 for Monday.
+            int y = (int)date.DayOfWeek + 1; // y is 1 for Sunday, 2 for Monday
+
+            int x = 0; // x is Burmese Calendar year
+            if (date > new DateOnly(date.Year, 4, 17)) // check if the date is after Burmese New Year
+            {
+                x = date.Year - 637;
+            }
+            else
+            {
+                x = date.Year - 638;
+            }
+            int z = 0; // z is the Mahaboard result number
+            z = (( (7 - x) + (y % 7)) *2) % 7;
+            await Task.Delay(1);
+            return "မွေးရက်";
+        }
+    }
+}
---
+++ MB.Models/Maharboard.cs
@@ -0,0 +1,27 @@
+﻿namespace MB.Model
+{
+    public class Mahaboard
+    {
+        /// <summary>
+        /// Birthdate shall be in UTC format with ISO 8601 standard.
+        /// E.g. yyyy-MM-ddTHH:mm:ssZ
+        /// </summary>
+        public DateOnly BirthDate { get; set; }
+        /// <summary>
+        /// BirthDay is the Person birth day such as Monday, Tuesday, Wednesday and so on.
+        /// </summary>
+        public string BirthDay { get; set; } = "မွေးရက်";
+        /// <summary>
+        /// MahaboardName is the name of Maharboard Result such as Adipadi, Raza, Puti and so on.
+        /// </summary>
+        public string MahaboardName { get; set; } = "အဖွားအမည်";
+        /// <summary>
+        /// MahaboardResult is the result of Mahaboard.
+        /// </summary>
+        string MahaboardResult { get; set; } = "မဟာဘုတ် အဟော";
+        /// <summary>
+        /// Gender of the Person.
+        /// </summary>
+        string Gender { get; set; } = "ယောက်ျား/မိန်းမ";
+    }
+}
