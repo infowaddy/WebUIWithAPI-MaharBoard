64748c4847f45d0267bb1087019d44364a255ad0
Zin Min <zinmin@hotmail.com>
2025-02-28 10:51:21 -0600

    delete unnecessary project

--- MB.WebApi/Controllers/WeatherForecastController.cs
+++ MB.WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace MB.WebApi.Controllers
 {
@@ -29,5 +30,39 @@ namespace MB.WebApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("/MyCustomer")]
+        public async Task<IActionResult> GetSomething(string getparam)
+        {
+            // do something
+            return await Task.Run(() => StatusCode((int)HttpStatusCode.OK, $"this is getparam " + getparam));
+        }
+
+        [HttpPost]
+        [Route("/MyCustomer")]
+        public async Task<IActionResult> PostSomething(MyCustomer mycustomer)
+        {
+            // do something
+            mycustomer.Status = ":Server Side Update";
+            return await Task.Run(() => StatusCode((int)HttpStatusCode.OK, mycustomer));
+        }
+
+        [HttpDelete]
+        [Route("/MyCustomer")]
+        public async Task<IActionResult> PutSomething(MyCustomer mycustomer)
+        {
+            // do something
+            mycustomer.Status = ":Server Side Deleted";
+            return await Task.Run(() => StatusCode((int)HttpStatusCode.OK, mycustomer));
+            
+        }
+    }
+
+    public class MyCustomer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
     }
 }
